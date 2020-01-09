2e8a4eebdf3f0fb1491240d86d5cbab426012a35
Danniedannie <danielle.simone.lynch@gmail.com>
2020-01-09 22:12:17 +0000

    Add webpack basic configs

--- src/server/index.js
+++ src/server/index.js
@@ -3,9 +3,9 @@ dotenv.config()
 var path = require('path')
 var aylien = require('aylien_textapi')
     //set aylien API credentials
-var textapi = new AYLIENTextAPI({
+var textapi = new aylien({
     application_id: process.env.API_ID,
-    application_key: process.end.API_KEY
+    application_key: process.env.API_KEY
 });
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
@@ -21,7 +21,7 @@ app.get('/', function(req, res) {
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function() {
+app.listen(3000, function() {
     console.log('Example app listening on port 8080!')
 })
 
