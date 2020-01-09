700ba16637a0b31e48c050a835183b801d295e72
Danniedannie <danielle.simone.lynch@gmail.com>
2020-01-09 21:46:46 +0000

    Add env variables and dotenv for Aylien API use

--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,12 @@
+const dotenv = require('dotenv')
+dotenv.config()
 var path = require('path')
+var aylien = require('aylien_textapi')
+    //set aylien API credentials
+var textapi = new AYLIENTextAPI({
+    application_id: process.env.API_ID,
+    application_key: process.end.API_KEY
+});
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 
@@ -8,15 +16,15 @@ app.use(express.static('dist'))
 
 console.log(__dirname)
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     res.sendFile('dist/index.html')
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
+app.listen(8080, function() {
     console.log('Example app listening on port 8080!')
 })
 
-app.get('/test', function (req, res) {
+app.get('/test', function(req, res) {
     res.send(mockAPIResponse)
 })
