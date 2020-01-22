156e39b0edd7d0af2279acea1e81bf369c8d8220
Jeremy Cobb <jeremy.cobb@shopify.com>
2020-01-22 08:35:49 -0500

    get a base version working

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,41 +1,16 @@
-var aylien = require('aylien_textapi')
-    //set aylien API credentials
-var textapi = new aylien({
-    application_id: 'a49e5674',
-    application_key: 'd80d3cdb1e1d27f79cb58c448878207'
-});
+// how can this be made into a class?
+// in the futre you would pass the form data
+async function handleSubmit(event) {
+  event.preventDefault();
+  await fetch("/test", {
+    method: "POST"
+  });
+}
 
-textapi.sentiment({ 'text': 'John is a very good football player!' }, function(error, response) {
-    console.log('test')
-    if (error === null) {
-        console.log(response);
-    } else {
-        console.log(error);
-    }
-});
+async function getAnalysis() {
+  const request = await fetch("/all");
+  const data = await request.json();
+  console.log("got it", data);
+}
 
-
-function handleSubmit(event) {
-    event.preventDefault()
-
-    // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    Client.checkForName(formText)
-    console.log(formText)
-
-    console.log("::: Form Submitted :::")
-        // fetch(textapi.sentiment.)
-        // .then((response) => {
-        //   return response.json();
-        // })
-        // .then((myJson) => {
-        //   console.log(myJson);
-};
-
-//document.getElementById('results').innerHTML = res.message
-
-
-
-
-
-export { handleSubmit }
+export { handleSubmit, getAnalysis };
